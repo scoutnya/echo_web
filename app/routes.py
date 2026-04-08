e22cb0d7c3fdb929c972d847fb53a3722899839b
scoutnya <aaknignts@gmail.com>
2026-04-07 23:51:29 -0700

    day 2

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,6 @@ main = Blueprint('main', __name__, template_folder='../templates')
 @main.route("/")
 def index():
     return render_template("index.html")
+@main.route("/post")
+def post():
+    return render_template("post.html")
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+print("RUNNING")
+
 from app import create_app
 
 app = create_app()
